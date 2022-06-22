a23645cadf88769632b603c2d1282ebd13a1883b
JesseSacramento <21938@stu.ipbeja.pt>
2022-06-22 14:48:03 +0100

    Threads estão funcionando precisando apenas de alguns arranjos

--- src/pt/ipbeja/po2/chartracer/gui/BarChartRacer.java
+++ src/pt/ipbeja/po2/chartracer/gui/BarChartRacer.java
@@ -1,29 +1,18 @@
 
 package pt.ipbeja.po2.chartracer.gui;
 
-import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import pt.ipbeja.po2.chartracer.model.BarModel;
 import pt.ipbeja.po2.chartracer.model.City;
-import javafx.scene.control.Label;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -31,131 +20,141 @@ import java.util.Map;
  * @author Jessé Sacramento
  * @version 20/05/2022
  */
-public class BarChartRacer {
+public class BarChartRacer extends Thread {
     private final double WIDTH = 900;
     private final double HEIGHT = WIDTH / 1.5;
     City city;
+    private List<String> file = new ArrayList<>();
+    private Pane pane;
+    private int num;
+    Thread thread;
+
+
+    public BarChartRacer(Pane pane, List<String> file) {
+        this.pane = pane;
+        this.file = file;
+        barRectangle(this.pane, this.file);
+    }
 
 
     public void barRectangle(Pane pane, List<String> file) {
 
-
-        List<City> cities; // list of cities
-        List<City> specificSet; // list of specific set
-
-        cities = City.citiesList(file); // list of the file
-
-        List<String> infoOfFirstColumn = City.findAllFirstData(cities); // info of the first information in each line
-
-        Map<String, Integer> numberOfCities = City.getNumberOfCities(file); // get the number of cities
-        int number = numberOfCities.get(infoOfFirstColumn.get(0));
-
-        double height = HEIGHT / number;
-        double x = 2;
-        double y = 0; // y start at zero
-        int space = (int) ((HEIGHT / number) / number);
-
-        City.sortCities(cities); // sort the list of cities
-
-        specificSet = City.getSpecificSet(cities, "2018"); // specific set
-
-        double maxValue = City.getMaxValue(specificSet);
+        this.thread = new Thread(() -> {
+            List<City> cities; // list of cities
+            List<City> specificSet; // list of specific set
 
 
-        for (int i = 0; i < number; i++) {
+            cities = City.citiesList(file); // list of the file
 
-            double width = getTheSpecificSpace(maxValue, HEIGHT, (specificSet.get(i).getQty()));
+            for (int i = 0; i < 518; i++) {
 
-            this.city = new City(specificSet);
-            List<Integer> numbersColor = city.getNumbers();
+                List<String> infoOfFirstColumn = City.findAllFirstData(cities); // info of the first information in the first column of each line
 
-            Color color = Color.rgb(((numbersColor.get(i) * number) % 255)
-                    ,(numbersColor.get(i) * number) % 255,numbersColor.get(i)); // generate The color to the barChart
+                Map<String, Integer> numberOfCities = City.getNumberOfCities(file); // get the number of cities
+                int number = numberOfCities.get(infoOfFirstColumn.get(i));
 
-            // the space between the bars used in the sub width-space = to reduce the width and get some
-            //space
-            createRectangleWithText(pane, x, y, width, height - space, color,
-                    specificSet.get(i).getCityName());
-            y += (HEIGHT / number);
-        }
+                double height = HEIGHT / number;
+                double x = 10;
+                double y = 0; // y start at zero
+                int space = (int) ((HEIGHT / number) / number);
+
+                City.sortCities(cities); // sort the list of cities
+
+                specificSet = City.getSpecificSet(cities, infoOfFirstColumn.get(i)); // specific set
+
+                double maxValue = City.getMaxValue(specificSet);
+
+
+                for (int j = 0; j < number; j++) {
+
+                    double width = getTheSpecificSpace(maxValue, HEIGHT, (specificSet.get(j).getQty()));
+
+                    this.city = new City(specificSet);
+                    List<Integer> numbersColor = city.getNumbers();
+
+                    Color color = Color.rgb(((numbersColor.get(j) * number) % 255)
+                            , (numbersColor.get(j) * number) % 255, numbersColor.get(j)); // generate The color to the barChart
+
+                    // the space between the bars used in the sub width-space = to reduce the width and get some
+                    //space
+                    createRectangleWithText(pane, x, y, width, height - space, color,
+                            specificSet.get(j).getCityName());
+
+
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    y += (HEIGHT / number);
+                }
+            }
+        });
+        thread.start();
+
     }
 
 
     /**
-     * @param pane pane to show the draw
-     * @param x x position
-     * @param y y position
+     * @param pane   pane to show the draw
+     * @param x      x position
+     * @param y      y position
      * @param height the height of the rectangle
      * @param width  the width of the rectangle
      * @param color  color to fill the rectangle
      * @param text   text to show the name
      */
     public void createRectangleWithText(Pane pane, double x, double y,
-                                               double width, double height, Color color, String text) {
+                                        double width, double height, Color color, String text) {
+        Platform.runLater(() -> {
 
-        Rectangle rectangle = new Rectangle(x, y, width, height );
-        rectangle.setStroke(Color.BLACK);
-        rectangle.setFill(color);
+            Rectangle rectangle = new Rectangle(x, y, width, height);
+            rectangle.setStroke(Color.BLACK);
+            rectangle.setFill(color);
 
-        // Text
-        Text txt = new Text(text);
-        txt.prefHeight(height);
-        txt.setFont(Font.font(25));
-        txt.setTextAlignment(TextAlignment.RIGHT);
+            // Text
+            Text txt = new Text(text);
+            txt.prefHeight(height);
+            txt.setFont(Font.font(25));
+            txt.setTextAlignment(TextAlignment.RIGHT);
 
-        // Vbox to put the text inside
-        VBox vBox = new VBox(txt);
-        vBox.setLayoutX(x);
-        vBox.setLayoutY(y);
-        vBox.setPrefHeight(height);
-        vBox.setPrefWidth(width);
-        vBox.setAlignment(Pos.CENTER_RIGHT);
+            // Vbox to put the text inside
+            VBox vBox = new VBox(txt);
+            vBox.setLayoutX(x);
+            vBox.setLayoutY(y);
+            vBox.setPrefHeight(height);
+            vBox.setPrefWidth(width);
+            vBox.setAlignment(Pos.CENTER_RIGHT);
 
-        // insert in pane
-        pane.getChildren().addAll(rectangle, vBox);
+            // insert in pane
+            pane.getChildren().addAll(rectangle,vBox);
+        });
     }
 
 
     /**
      * @param maxValue the max value that is present in the file
      * @param height   the height of the pane
-     * @param value        the qty that we get from the class City
+     * @param value    the qty that we get from the class City
      * @return return the
      */
     public double getTheSpecificSpace(double maxValue, double height, double value) {
         return (height * value) / maxValue;
     }
 
+
+    // getters
+
     /**
-     *
-     * @param stage the stage to show the file
-     * @return return a List<String> of the chosen file
+     * @return return the list file
      */
-    public List<String> chooseTheFile(Stage stage) {
-        FileChooser chooser = new FileChooser();
-        chooser.setTitle("Escolha O ficheiro");
-        List<String> file = new ArrayList<>();
-
-        chooser.getExtensionFilters().add(
-                new FileChooser.ExtensionFilter("Ficheiros de texto", "*.txt"));
-
-        File initialDirectory = new File(".");
-
-        chooser.setInitialDirectory(initialDirectory);
-
-        File db = chooser.showOpenDialog(stage);
-
-        try {
-            file = Files.readAllLines(Paths.get(db.getAbsolutePath()));
-
-            // transform in a list
-
-        } catch (IOException e) {
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Não foi possível abrir o ficheiro");
-            alert.show();
-            e.printStackTrace();
-        }
+    public List<String> getFile() {
         return file;
+    }
+
+    public Pane getPane() {
+        return pane;
     }
 
 }
